1c727912d80630c340d206c318b0c8227303a840
rogalski06 <rogalski@hawaii.edu>
2026-02-01 13:26:57 -1000

    Update Ex1.py

--- Lab 4/Ex1.py
+++ Lab 4/Ex1.py
@@ -6,3 +6,16 @@ last = input("Enter your last name: ")
 
 full_name = first + " " + middle + ". " + last
 print("Your full name is:", full_name)
+
+print(f"Your full name is: {first} {middle}. {last}")
+
+print("Your full name is: %s %s. %s" % (first, middle, last))
+
+print("Your full name is: {} {}. {}".format(first, middle, last))
+
+parts = [first, middle, last]
+print("Your full name is: {} {}. {}".format(*parts))
+
+parts_join = [first, middle + '.', last]
+print("Your full name is:", " ".join(parts_join))
+
