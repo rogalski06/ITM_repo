8febc544ea2c8c6fdbe99d0eb271bfd27c2cd63c
rogalski06 <rogalski@hawaii.edu>
2026-02-05 12:35:53 -1000

    Update Ex1.py

---
+++ Lab 5/Ex1.py
@@ -0,0 +1,9 @@
+taxi_trip_info = {
+    "trip_id": "da7a62fce",
+    "trip_seconds": 360,
+    "trip_miles": 1.1,
+    "fare": "$6.25"}
+
+print(taxi_trip_info)
+print(taxi_trip_info["trip_miles"])
+
