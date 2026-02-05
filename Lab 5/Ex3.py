5f8d6604291fd390dff33c402871e1c636c32e09
rogalski06 <rogalski@hawaii.edu>
2026-02-05 13:15:34 -1000

    Update Ex3.py

--- Lab 5/Ex3.py
+++ Lab 5/Ex3.py
@@ -4,4 +4,4 @@ respondent_ids = (1012, 1035, 1021, 1053)
 survey_dict = dict(zip(respondent_ids, responses))
 print("Survey responses with respondent IDs:", survey_dict)
 
-print(f"Respond {respondent_ids[2]} gave a response of {survey_dict[respondent_ids[2]]}.")
+print(f"Respondent {respondent_ids[2]} gave a response of {survey_dict[respondent_ids[2]]}.")
