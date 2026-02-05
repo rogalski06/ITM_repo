fe977ad05066e8dd6b862cbdf4eedcaa42fb706b
rogalski06 <rogalski@hawaii.edu>
2026-02-05 13:08:46 -1000

    Update Ex2.py

--- Lab 5/Ex2.py
+++ Lab 5/Ex2.py
@@ -1,6 +1,16 @@
 trip_durations = [1.1, 0.8, 2.5, 2.6]
 trip_fares = (6.25, 5.25, 10.50, 8.05)
 
+trips_list = [
+    {"duration": 1.1, "fare": 6.25},
+    {"duration": 0.8, "fare": 5.25},
+    {"duration": 2.5, "fare": 10.50},
+    {"duration": 2.6, "fare": 8.05}
+]
+
+print("List of trip dictionaries:")
+print(trips_list)
+
 taxi_trips = {
     "miles": trip_durations,
     "fares": trip_fares
