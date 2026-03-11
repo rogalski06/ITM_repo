e5ab4ed47ac1b9ce9a72b5619f2b2317b724ce7f
rogalski06 <rogalski@hawaii.edu>
2026-03-10 15:00:30 -1000

    Update Ex5.py

--- Lab 8-b/Ex5.py
+++ Lab 8-b/Ex5.py
@@ -1,8 +1,10 @@
 # Program to remove any scores from a list that are below 50.
  
 scores = [60, 45, 30, 85, 10, 90] 
+scores_list = []
 
-for score in scores: 
-    if score < 50: 
-        scores.remove(score) 
-print(scores) 
+while scores: 
+    score = scores.pop(0) 
+    if score >= 50: 
+        scores_list.append(score)
+print(scores_list)
