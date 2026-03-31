d2fe5e01ffb58e492127d00b160ba9773b2891bc
rogalski06 <rogalski@hawaii.edu>
2026-03-31 13:01:05 -1000

    Update Ex2.py

--- Lab 11/Ex2.py
+++ Lab 11/Ex2.py
@@ -10,19 +10,18 @@ filename = "https://drive.google.com/uc?id=1ujY0WCcePdotG2xdbLyeECFW9lCJ4t-K"
 pd.set_option('display.max_columns', None)  # Show all columns in the output
 
 df = pd.read_csv(filename, engine='pyarrow')
-df['order_date'] = pd.to_datetime(df['order_date'], errors='coerce')
+df['order_date'] = pd.to_datetime(df['order_date'], format='%m/%d/%Y', errors='coerce')
 
 # Coerce quantity and unit_price to numeric, setting errors to NaN
 df['quantity'] = pd.to_numeric(df['quantity'], errors='coerce')
 df['unit_price'] = pd.to_numeric(df['unit_price'], errors='coerce')
 df['sales'] = df['quantity'] * df['unit_price']
 
-pivot_table = df.pivot_table(df,
+pivot_table = pd.pivot_table(df,
                              index='sales_region',
+                             values='sales',
                              columns='order_type',
                              aggfunc=np.sum,
                              margins=True,
                              margins_name='Total Sales')
-
-
-print(df.head(5))
+print(pivot_table)
