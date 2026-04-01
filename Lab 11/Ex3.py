536a28fd828d0fbfe1ff6c635b4d5bff40bedd7b
rogalski06 <rogalski@hawaii.edu>
2026-04-01 13:56:34 -1000

    Update Ex3.py

--- Lab 11/Ex3.py
+++ Lab 11/Ex3.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 import pyarrow
+import ssl
 
 filename = "https://drive.google.com/uc?id=1ujY0WCcePdotG2xdbLyeECFW9lCJ4t-K"
 
@@ -19,13 +20,11 @@ df['quantity'] = pd.to_numeric(df['quantity'], errors='coerce')
 df['unit_price'] = pd.to_numeric(df['unit_price'], errors='coerce')
 df['sales'] = df['quantity'] * df['unit_price']
 
-state_col = 'customer_state'
-
 pivot_table = pd.pivot_table(df,
-                             index='sales_region',
+                             index='customer_state',
                              values='sales',
-                             columns=['order_type', state_col], 
-                             aggfunc=[np.sum, np.mean],
+                             columns=['customer_type', 'order_type'], 
+                             aggfunc=np.mean,
                              margins=True,
                              margins_name='Total Sales')
 print(pivot_table)
